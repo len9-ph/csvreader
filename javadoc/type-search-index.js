8a33255cc8bc30d000ae2e8eee6c8f308da967c5
Leonid Gadetsky <leonidgadetsky@yandex.ru>
2021-12-20 11:46:43 +0300

    add javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"InputOutput","l":"CsvReader"},{"p":"InputOutput","l":"CsvReaderTest"},{"p":"entities","l":"Department"},{"p":"entities","l":"Human"},{"p":"<Unnamed>","l":"main"}];updateSearchResults();
